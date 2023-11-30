4dd82be786db50c8b3ecc0ad27e52928290b41b6
ABHINAV <abhinavpatel0208@gmail.com>
2023-11-30 17:43:50 +0530

    Completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <p className="text-base font-bold text-white">@2023 AnimeVault</p>
       <Image
         src="./logo.svg"
         alt="logo"
@@ -12,13 +12,6 @@ function Footer() {
         className="object-contain"
       />
       <div className="flex items-center gap-6">
-        <Image
-          src="./tiktok.svg"
-          alt="logo"
-          width={19}
-          height={19}
-          className="object-contain"
-        />
         <Image
           src="./instagram.svg"
           alt="logo"
